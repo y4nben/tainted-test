88c2baa848cba808a36bd8879d22081a81848d85
Ben Yan <87666186+y4nben@users.noreply.github.com>
2022-07-20 09:30:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "tainted-tf" {
-  source     = "github.com/y4nben/terraform-google-cloud-storage"
+module "terraform_test_bucket" {
+  source     = "github.com/y4nben/terraform-google-cloud-storage" #change to gcs before deploy
   project_id = var.project_id
   prefix     = "storage"
-  names      = ["pot-of-duality-123"]
+  names      = ["pot-of-duality"]
   location   = "US"
-  versioning = { "pot-of-duality-123" = true }
+  versioning = { "pot-of-duality" = true }
 }
