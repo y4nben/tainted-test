6600b213b4ff45dabca908b25c31d7c986800738
Ben Yan <87666186+y4nben@users.noreply.github.com>
2022-07-20 09:32:07 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-module "terraform_test_bucket" {
-  source     = "github.com/y4nben/terraform-google-cloud-storage" #change to gcs before deploy
-  project_id = var.project_id
-  prefix     = "storage"
-  names      = ["pot-of-duality"]
-  location   = "US"
-  versioning = { "pot-of-duality" = true }
+terraform {
+  backend "gcs" {
+    bucket = "pot-of-backend"
+    prefix = "state/"
+  }
 }
